a0f5d727c63890aef1e3964d1cf65a4e74d84542
greybubble <greybubble@gmail.com>
2026-03-09 11:41:47 -0500

    Update documentation

--- localbuild.sh
+++
@@ -1,8 +0,0 @@
-#Convert Jupyter notebooks to markdown and load them into the content folder
-find notebooks/ -type f | xargs ./convert_nb.sh
-
-#Run the static site generator
-pelican content
-
-#Run the preview server for the site
-pelican --listen
